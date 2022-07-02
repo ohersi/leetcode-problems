8f912b4bfd30c95ae39ab2201644d0e84dc275b6
Omar Hersi <o.hersi@yahoo.com>
2022-07-01 22:48:51 -0400

    121. Best Time to Buy and Sell Stock - v2 sightly condensed

--- src/LeetCode/BestTimeToBuyAndSellStock.java
+++ src/LeetCode/BestTimeToBuyAndSellStock.java
@@ -2,25 +2,15 @@ package LeetCode;
 
 public class BestTimeToBuyAndSellStock {
     public static int[] price = {7,1,5,3,6,4};
-    public static void bruteForce(int[] price) {
+    public static int slidingWindow(int[] price) {
         // Time complexity of this algorithm is 0(N) - length of the price array
         // Space complexity is 0(1) - no extra space other than variables; number of variables remains constant
         int left = 0;
         int right = left + 1;
         int profit = 0;
 
-        if (price.length <= 1) {
-            System.out.println("profit is:" + profit);
-        }
-
-        if (right == price.length - 1) {
-            if (price[right] - price[left] > profit) {
-                profit = price[right] - price[left];
-            }
-        }
-
-        while (right < price.length - 1) {
-            // Check initial right - left is greater than profit
+        while (right < price.length) {
+            // Check right - left is greater than profit
             if (price[right] - price[left] > profit) {
                 profit = price[right] - price[left];
             }
@@ -31,17 +21,12 @@ public class BestTimeToBuyAndSellStock {
             }
             // Move right pointer (selling price) to maximize profit, higher the number compared to buying price the better
             right += 1;
-            // Check new right - left is greater than profit
-            if (price[right] - price[left] > profit) {
-                profit = price[right] - price[left];
-            }
-            System.out.println("left: " + price[left]);
-            System.out.println("right: " + price[right]);
         }
-        System.out.println("profit: " + profit);
+        return profit;
     }
 
+
     public static void main(String[] args) {
-        bruteForce(price);
+        System.out.println("Maximum profit: " + slidingWindow(price));
     }
 }
